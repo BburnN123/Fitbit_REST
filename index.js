b554fd8795030c5c0aeb68c106ed3139b4c951ca
BburnN123 <huijie_1995@hotmail.com>
2020-09-16 02:49:15 +0800

    test

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 console.log("test")
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host = "us-cdbr-east-02.cleardb.com",
-  user = "b2c53c3f94729e",
-  password = "ed90f947",
-});
+// var con = mysql.createConnection({
+//   host = "us-cdbr-east-02.cleardb.com",
+//   user = "b2c53c3f94729e",
+//   password = "ed90f947",
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
