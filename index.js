17d9eee9217637598b5b46890bda35aa02a0f103
BburnN123 <huijie_1995@hotmail.com>
2020-09-16 02:49:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log("test")
 
-// var mysql = require('mysql');
+var mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//   host = "us-cdbr-east-02.cleardb.com",
-//   user = "b2c53c3f94729e",
-//   password = "ed90f947",
-// });
+var con = mysql.createConnection({
+  host = "us-cdbr-east-02.cleardb.com",
+  user = "b2c53c3f94729e",
+  password = "ed90f947",
+});
 
 // con.connect(function(err) {
 //   if (err) throw err;
