829fcb3a349ea39486de871cf9ff29f49fdeab5b
BburnN123 <huijie_1995@hotmail.com>
2020-09-16 02:44:29 +0800

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host = "us-cdbr-east-02.cleardb.com",
+  user = "b2c53c3f94729e",
+  password = "ed90f947",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
