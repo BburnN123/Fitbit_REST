41c418ab598d67d3dd2f6e5ec2c3dcdaa88356a7
BburnN123 <huijie_1995@hotmail.com>
2020-09-16 08:49:24 +0800

    Server Creation

--- api/routes/fitbit.js
+++ api/routes/fitbit.js
@@ -1,39 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-const sql = require("mssql");
+var mysql = require("mysql");
 
 // config for your database
-const config = {
+var con = mysql.createConnection({
     user: 'b2c53c3f94729e',
     password: 'ed90f947',
-    server: 'us-cdbr-east-02.cleardb.com', 
-    database: 'heroku_3dc17b7d0a2d5d3' 
-};
-
-router.get('/', (req, res, next) => {
-   
+    host: 'us-cdbr-east-02.cleardb.com', 
+    // database: 'heroku_3dc17b7d0a2d5d3' 
 });
 
-router.post('/', (req, res, next) => {
-    // connect to your database
+router.post('/accel', (req, res, next) => {
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO heroku_3dc17b7d0a2d5d3.accel (acc_time, acc_x, acc_y, acc_z, acc_sum) VALUES ?";
+        var values = [[req.query.time, req.query.x, req.query.y, req.query.z, req.query.sum]];
+        con.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+          });
+      });
+});
 
-    sql.connect(config, function (err) {
-        
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-        
-        // query to the database and get the records
-        request.query('INSERT INTO heroku_3dc17b7d0a2d5d3.accel (acc_time, acc_json) VALUES ("1", "2")', function (err, recordset) {       
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
+router.post('/gyro', (req, res, next) => {
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO heroku_3dc17b7d0a2d5d3.gyro (gyro_time, gyro_x, gyro_y, gyro_z, gyro_sum) VALUES ?";
+        var values = [[req.query.time, req.query.x, req.query.y, req.query.z, req.query.sum]];
+        con.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+          });
+      });
 });
 
 module.exports = router;
