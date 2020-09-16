643a3dbc8cafcda6a123d7513b5dd80a1c578e53
BburnN123 <huijie_1995@hotmail.com>
2020-09-16 09:17:00 +0800

    Update fitbit.js

--- api/routes/fitbit.js
+++ api/routes/fitbit.js
@@ -5,18 +5,18 @@ var mysql = require("mysql");
 
 // config for your database
 var con = mysql.createConnection({
-    user: 'b2c53c3f94729e',
-    password: 'ed90f947',
+    user: 'bfd9884b2bf426',
+    password: '789f2ecb',
     host: 'us-cdbr-east-02.cleardb.com', 
-    // database: 'heroku_3dc17b7d0a2d5d3' 
+    // database: 'us-cdbr-east-02.cleardb.com/' 
 });
 
 router.post('/accel', (req, res, next) => {
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-        var sql = "INSERT INTO heroku_3dc17b7d0a2d5d3.accel (acc_time, acc_x, acc_y, acc_z, acc_sum) VALUES ?";
-        var values = [[req.query.time, req.query.x, req.query.y, req.query.z, req.query.sum]];
+        var sql = "INSERT INTO heroku_5785e0fa76e8d8b.accel (accel_time, accel_json, accel_x, accel_y, accel_z, accel_sum) VALUES ?";
+        var values = [[req.query.time, req.query.json, req.query.x, req.query.y, req.query.z, req.query.sum]];
         con.query(sql, [values], function (err, result) {
             if (err) throw err;
             console.log("Number of records inserted: " + result.affectedRows);
@@ -28,8 +28,8 @@ router.post('/gyro', (req, res, next) => {
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-        var sql = "INSERT INTO heroku_3dc17b7d0a2d5d3.gyro (gyro_time, gyro_x, gyro_y, gyro_z, gyro_sum) VALUES ?";
-        var values = [[req.query.time, req.query.x, req.query.y, req.query.z, req.query.sum]];
+        var sql = "INSERT INTO heroku_5785e0fa76e8d8b.gyro (gyro_time, gyro_json, gyro_x, gyro_y, gyro_z, gyro_sum) VALUES ?";
+        var values = [[req.query.time, req.query.json, req.query.x, req.query.y, req.query.z, req.query.sum]];
         con.query(sql, [values], function (err, result) {
             if (err) throw err;
             console.log("Number of records inserted: " + result.affectedRows);
